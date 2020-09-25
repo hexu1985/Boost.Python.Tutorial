08bf2813aea5bd31389ebc1ae32c893aa5ccb61a
hexu <hexu_1985@sina.com>
2020-09-25 11:37:07 +0800

    update chapter-02/recipe-01

--- chapter-02/recipe-01/src/build.sh
+++ chapter-02/recipe-01/src/build.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 
 rm -rf ./build
-mkdir build
-cd build
-cmake ..
-cmake --build .
-ctest
+#mkdir build
+#cd build
+#cmake ..
+#cmake --build .
+#ctest
+
+cmake -H. -Bbuild -DENABLE_UNIT_TESTS=ON
+cmake --build build
+
+
